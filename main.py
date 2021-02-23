9c9538e6d3b4b1ca319181d6003ec5e8ceca4cdf
Maciej, Zalwert <maciej.zalwert@gmail.com>
2021-02-23 17:19:43 +0100

    Add template for other examples

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import re
 
+
+# Example 1: finding e-mails in text
 example_string = 'This is example string with example@example.com mail ' \
     'and with example-example@example.com.'
 
 all_emails = re.findall(r'[\w.-]+@[\w.-]+.\w+', example_string)
 
 print(all_emails)
+
+# Example 2:
+# Example 3:
+# Example 4:
+# Example 5:
